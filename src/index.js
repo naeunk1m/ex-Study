e7825bfeae87906598eab144fc89893d182d3822
naeunk1m <08enmik@gmail.com>
2022-12-29 06:29:10 +0000

    Update 8 files

--- src/index.js
+++ src/index.js
@@ -12,12 +12,23 @@ function taskB(a, cb) {
   }, 1000);
 }
 
+function taskC(a, cb) {
+  setTimeout(() => {
+    const res = a * -1;
+    cb(res);
+  }, 2000);
+}
+
 taskA(3, 4, (res) => {
   console.log("A TASK RESULT : ", res);
 });
 
 taskB(7, (res) => {
   console.log(" B TASK RESULT : ", res);
+});
+
+taskC(14, (res) => {
+  console.log(" C TASK RESULT : ", res);
 });
 
 console.log("코드 끝");
